b0e93b6be879a0ea0802891ecc9c7ff6592d5953
menoutep <josephzabre@gmail.com>
2024-02-19 10:03:42 +0000

    docker

--- base/admin.py
+++ base/admin.py
@@ -5,6 +5,7 @@ from simple_history.admin import SimpleHistoryAdmin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from accounts.forms import CustomUserUpdateForm
 from base.forms import BackupStrategieUpdateForm
+from base import forms
 from accounts.models import User
 # Register your models here.
 #@admin.register(BackupStrategie)
@@ -21,7 +22,7 @@ admin.site.register(User, UserAdmin)
 
 
 class BackupStrategieAdmin(SimpleHistoryAdmin):
-    list_filter = ('name', 'file', 'updated')
+    list_filter = ('file', 'updated')
     history_list_display = ["file"]
     search_fields = ['name', 'file','applications__name']
     empty_value_display = "unknown"
@@ -29,25 +30,49 @@ class BackupStrategieAdmin(SimpleHistoryAdmin):
 admin.site.register(BackupStrategie, BackupStrategieAdmin)
 class DepartementAdmin(SimpleHistoryAdmin):
     list_filter = ('name','updated','created')
-    history_list_display = ["name","description","users_name"]
-    search_fields = ["name","description","users_name"]
+    history_list_display = ["name","description","users__name"]
+    search_fields = ["name","description","users__name"]
+    empty_value_display = "unknown"
+    form = forms.DepartementUpdateForm
 admin.site.register(Departement, DepartementAdmin)
 
 class AppTypeAdmin(SimpleHistoryAdmin):
     list_filter = ('name','updated','created')
-    history_list_display = ["name","description","applications_name"]
-    search_fields = ["name","description","applications_name"]
+    history_list_display = ["name","description","applications__name"]
+    search_fields = ["name","description","applications__name"]
+    empty_value_display = "unknown"
+    form = forms.AppTypeUpdateForm
 admin.site.register(AppType, AppTypeAdmin)
 
 #####Application start######################
+class ApplicationAdmin(SimpleHistoryAdmin):
+    list_filter = ('deployement_year','app_type','updated','created')
+    history_list_display = ["name","description","replication","priority","control_name","deployement_year","app_type","backup_strategie"]
+    search_fields = ["name","description","modules_applicatifs__name","technicals_recoveries_plans__name","backup_strategie"]
+    empty_value_display = "unknown"
+    form = forms.ApplicationUpdateForm
+admin.site.register(Application, ApplicationAdmin)
+
+class DatabaseServerAdmin(SimpleHistoryAdmin):
+    list_filter = ('updated','created')
+    history_list_display = ["name","ram","rom","cluster","server"]
+    search_fields = ["name",'databases_name',"description","cluster__name","cluster__ip_address__ipv4","server__name"]
+    empty_value_display = "unknown"
+    form = forms.DatabaseServerUpdateForm
+admin.site.register(DatabaseServer, DatabaseServerAdmin)
+
 
 #####Application end#############################
 
-#admin.site.register(BackupStrategie)
-admin.site.register(Application)
-admin.site.register(Database)
-admin.site.register(DatabaseServer)
-admin.site.register(SystemeStockage)
+
+class SystemeStockageAdmin(SimpleHistoryAdmin):
+    list_filter = ('updated','created')
+    history_list_display = ["name","ram","rom"]
+    search_fields = ["name",'ram','rom']
+    empty_value_display = "unknown"
+    form = forms.SystemeStockageUpdateForm
+admin.site.register(SystemeStockage, SystemeStockageAdmin)
+
 admin.site.register(Vendor)
 admin.site.register(ModuleApplicatif)
 admin.site.register(Server)
--- dockerfile
+++ dockerfile
@@ -1,30 +1,54 @@
-# Utiliser l'image officielle Python 3.9 comme base
-FROM python:3.9
-
-# Mettre en place les variables d'environnement pour éviter les avertissements de Python
-ENV PYTHONDONTWRITEBYTECODE 1
+FROM ubuntu:22.04
+RUN apt-get update
+RUN apt-get install -y apt-utils vim curl apache2 apache2-utils apache2-dev 
+RUN apt-get -y install python3 libapache2-mod-wsgi-py3
+RUN ln /usr/bin/python3 /usr/bin/python
+RUN apt-get -y install python3-pip
+RUN apt-get install w3m -y
+RUN pip install --upgrade pip
+RUN pip install django ptvsd
 ENV PYTHONUNBUFFERED 1
+RUN mkdir /cmdbeas/
+WORKDIR /cmdbeas/
 
-# Définir le répertoire de travail dans le conteneur
-RUN  mkdir /app
-WORKDIR /app
-# Copier le contenu de l'application dans le conteneur
-COPY . /app/
-
-# Copier le fichier requirements.txt et installer les dépendances
-COPY requirements.txt /app/
-COPY cmdbeas/.env /app/cmdbeas/
-# Créer un environnement virtuel et installer les dépendances
-RUN python -m venv /env
+COPY . /cmdbeas/
+RUN apt-get install -y python3-venv
+RUN python3 -m venv /env
 ENV PATH="/env/bin:$PATH"
-COPY entrypoint.sh /app/entrypoint.sh
-#RUN chmod +x /app/entrypoint.sh 
 
-
-RUN /env/bin/python -m pip install --upgrade pip
+RUN /env/bin/python3 -m pip install --upgrade pip
 RUN /env/bin/pip install --no-cache-dir -r requirements.txt
+RUN /env/bin/pip install  pip install mod-wsgi
+COPY ./apache/cmdbeas.conf /etc/apache2/sites-available/
 
 
+RUN chmod 664 /cmdbeas/db.sqlite3
+RUN chmod 775 /cmdbeas/cmdbeas
+RUN chmod 777 /cmdbeas
+RUN chown :www-data /cmdbeas/db.sqlite3
+RUN chown :www-data /cmdbeas/cmdbeas
+RUN a2dissite 000-default.conf
+#RUN echo "LoadModule wsgi_module '/usr/lib/apache2/modules/mod_wsgi.so'" >> /etc/apache2/mods-enabled/wsgi.load
+RUN echo 'LoadModule wsgi_module "/env/lib/python3.10/site-packages/mod_wsgi/server/mod_wsgi-py310.cpython-310-x86_64-linux-gnu.so"' > /etc/apache2/mods-available/wsgi.load
+RUN echo 'WSGIPythonHome "/usr"' >> /etc/apache2/mods-available/wsgi.load
 
-# Exécuter les migrations Django
+RUN a2ensite cmdbeas.conf
+RUN a2enmod proxy
+RUN echo $USER
+RUN a2enmod wsgi
+RUN a2enmod proxy_http
+RUN a2enmod proxy_balancer
+RUN a2enmod lbmethod_byrequests
+EXPOSE 80 3500
+#RUN source /env/bin/activate 
+RUN python3 manage.py makemigrations && python manage.py migrate
+
+# Commande pour créer un superutilisateur Django
+# Créer un superutilisateur Django avec un mot de passe spécifié
+
+# Lancement d'Apache après la création du superutilisateur
+#ENTRYPOINT ["/bin/bash", "-c", "apache2ctl -D FOREGROUND"]
+ENTRYPOINT ["/bin/bash"]
+
+
 
